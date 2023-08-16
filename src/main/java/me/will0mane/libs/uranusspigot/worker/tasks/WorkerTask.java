c9a22311c4b192bc76ddb48e4979764eb894ec71
Will0mane <100951314+Will0mane@users.noreply.github.com>
2023-08-16 20:05:52 +0200

    1.0.0.5

--- src/main/java/me/will0mane/libs/uranusspigot/worker/tasks/WorkerTask.java
+++ src/main/java/me/will0mane/libs/uranusspigot/worker/tasks/WorkerTask.java
@@ -2,6 +2,7 @@ package me.will0mane.libs.uranusspigot.worker.tasks;
 
 import lombok.Getter;
 import me.will0mane.libs.uranusspigot.worker.Worker;
+import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public abstract class WorkerTask extends BukkitRunnable {
@@ -23,5 +24,11 @@ public abstract class WorkerTask extends BukkitRunnable {
 
     public void setUsedID(int taskID){
         this.usedTaskID = taskID;
+    }
+
+    @Override
+    public synchronized void cancel() {
+        Bukkit.getScheduler().cancelTask(getTaskId());
+        worker.getTaskMap().remove(usedTaskID);
     }
 }
